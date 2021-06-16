8e3752bcca53cdd2775e75aeacfb6bcd8b38e126
rokpopov <rokpopov90@gmail.com>
2021-06-16 22:23:43 +0200

    creating a class to store html elements in variables

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 /**
  *  STEPS:
  *
- *  [] 1. Declare a class to GET weather data, and GET the woeid, output received data
+ *  [x] 1. Declare a class to GET weather data, and GET the woeid, output received data
  *  [] 2. Register an event listener and attach it to the GET requests chain from above, adjust UI loading state
  *  [] 3. Prepare data for the UI in advance and try to use unified structure before outputting to the template
  *  [] 4. Divide classes per function to have a more clean code approach and separation on concerns
@@ -33,9 +33,56 @@ class fetchForecastApi {
     }
 }
 
+class coreDomElements {
+    constructor() {
+        this.searchForm = $('#search-form');
+        this.errorBox = $('#error-box');
+        this.searchBox = $('#search-box');
+        this.loaderBox = $('#loader-box');
+        this.forecastBox = $('#forecast-box');
+    }
+
+    showForecast() {
+        this.hideError();
+        this.forecastBox.removeClass('d-none');
+        this.forecastBox.addClass('d-flex');
+    }
+
+    showLoader() {
+        this.loaderBox.removeClass('d-none');
+    }
+
+    hideLoader() {
+        this.loaderBox.addClass('d-none');
+    }
+
+    showSearch() {
+        this.searchBox.removeClass('d-none');
+        this.searchBox.addClass('d-flex');
+    }
+
+    hideSearchBox() {
+        this.searchBox.removeClass('d-flex');
+        this.searchBox.addClass('d-none');
+    }
+
+    showError(message) {
+        this.hideLoader();
+        this.showSearch();
+        this.errorBox.removeClass('d-none');
+        this.errorBox.addClass('d-block');
+        this.errorBox.html(`<p class="mb-8">${message}</p>`);
+    }
+
+    hideError() {
+        this.errorBox.addClass('d-none');
+    }
+}
+
 class requestController {
     constructor() {
         this.fetchForecastApi = new fetchForecastApi();
+        this.coreDomElements = new coreDomElements();
         this.init();
     }
 
